3bf15156d0ba3b9c3457ce4ea414c0fb1af0b163
Douglas.Y <siyuan.yang2@uq.net.au>
2023-06-02 01:17:21 +1000

    add three stack for the set of 2 buckets

--- bin/main.ts
+++ bin/main.ts
@@ -1,35 +1,36 @@
 #!/usr/bin/env node
 import "source-map-support/register"
 import * as cdk from "aws-cdk-lib"
-import { ServiceCRMStack } from "../src/ServiceCRMStack"
+import { createImageS3Bucket } from "../src/initialize/init_image_bucket";
+import { createDynamoDB } from "../src/initialize/init_ddb";
+import { createWebS3Bucket } from "../src/initialize/init_web_bucket";
 import { IConfig } from "../src/config"
 import { logger } from "../Shared/Utils/logger"
 
 const app = new cdk.App()
-const env = process.env.CRM_ENV ? process.env.CRM_ENV : "prod"
-logger.info("Env: ", env)
+// const env = process.env.CRM_ENV ? process.env.CRM_ENV : "prod"
+// logger.info("Env: ", env)
 
 // TODO: we need to copy different dev config
-const config: IConfig = app.node.tryGetContext("config")
+// const config: IConfig = app.node.tryGetContext("config")
+// const stackName = `JR-RealEstate-${config.environment}`
 
-const stackName = `JR-RealEstate-${config.environment}`
 
-new ServiceCRMStack(app, `${stackName}`, {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// initialize the three stacks for 2 buckets and 1 ddb table
+const web_stackName = "web-stack-20230601"
+const web_stack = new cdk.Stack(app, web_stackName);
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+const image_stackName = "image-stack-20230601";
+const image_stack = new cdk.Stack(app, image_stackName);
 
-  tags: {
-    Env: env,
-    StackName: stackName,
-  },
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-})
+const ddb_stackName = "ddb-stack-20230601";
+const ddb_stack = new cdk.Stack(app, ddb_stackName);
+
+
+// create the 3 resources
+const web_bucket = createWebS3Bucket(web_stack);
+const image_bucket = createImageS3Bucket(image_stack);
+const ddb = createDynamoDB(ddb_stack);
+
+
