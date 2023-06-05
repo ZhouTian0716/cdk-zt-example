a37ddb37a4e982ba083ca7a8dd0de56207849ce5
Libby <manyang.liu@uq.net.au>
2023-06-05 23:03:40 +1000

    change some data to real value

--- Database/insertInitialData.sh
+++ Database/insertInitialData.sh
@@ -4,13 +4,13 @@ aws dynamodb put-item --table-name CRM --item
 #'{"PK":{"S":"User"},"id":{"S":"1"},"email":{"S":"dave@cyberlark.com.au"},"phone":{"S":"0412173226"}}'
 '{
         "Address": {"S": "123 Main St"},
-        "Suburb": {"S": "Example Suburb"},
-        "Postcode": {"S": "12345"},
-        "State": {"S": "Example State"},
-        "CityCouncil": {"S": "Example City Council"},
+        "Suburb": {"S": "Woollonggabba"},
+        "Postcode": {"S": "2345"},
+        "State": {"S": "Queensland"},
+        "CityCouncil": {"S": "Brisbane"},
         "YearBuilt": {"N": "2005"},
         "Coordinates": {"M": {"Latitude": {"N": "37.1234"}, "Longitude": {"N": "-122.5678"}}},
-        "Agent": {"S": "example_username"},
+        "Agent": {"S": "Kevin"},
         "Bathrooms": {"N": "2"},
         "Bedrooms": {"N": "3"},
         "CarSpaces": {"N": "2"},
