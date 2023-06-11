c5d491300c5040703b85d01603800fb712720314
Douglas.Y <siyuan.yang2@uq.net.au>
2023-06-11 19:56:09 +1000

    ..

--- src/ServiceCRMStack.ts
+++ src/ServiceCRMStack.ts
@@ -1,7 +1,7 @@
 import * as Cdk from "aws-cdk-lib"
 import { Construct } from "constructs"
 import { createImageS3Bucket } from "./ImageBucketStack"
-import { createDynamoDB } from "./DynamoDBStack"
+import { createDynamoDB } from "./DynamodbStack"
 import { createWebS3Bucket } from "./WebBucketSatck"
 
 export class ServiceCRMStack extends Cdk.Stack {
