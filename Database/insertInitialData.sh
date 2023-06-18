95e48c56380423bed630e5633bd4ec2d46272bdf
Libby <manyang.liu@uq.net.au>
2023-06-18 22:47:57 +1000

    feature/02-add seed data

--- Database/insertInitialData.sh
+++ Database/insertInitialData.sh
@@ -5,6 +5,7 @@ aws dynamodb put-item \
     --item file://initialProperties.json \
     --return-consumed-capacity TOTAL \
     --return-item-collection-metrics SIZE 
+    
 
 # aws dynamodb put-item \
 #     --table-name Files \
