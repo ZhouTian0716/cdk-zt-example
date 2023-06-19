46c64b44969b9d4e9ba2706baf823213406c8b67
Libby <manyang.liu@uq.net.au>
2023-06-19 21:37:25 +1000

    save init seed data in one folder and it can be successfully add in db

--- Database/insertInitialData.sh
+++ Database/insertInitialData.sh
@@ -2,13 +2,43 @@ echo "Adding intial items ..."
 
 aws dynamodb put-item \
     --table-name Properties \
-    --item file://initialProperties.json \
+    --item file://initSeedData/Properties01.json \
+    --return-consumed-capacity TOTAL \
+    --return-item-collection-metrics SIZE 
+
+aws dynamodb put-item \
+    --table-name Properties \
+    --item file://initSeedData/Properties02.json \
+    --return-consumed-capacity TOTAL \
+    --return-item-collection-metrics SIZE 
+
+aws dynamodb put-item \
+    --table-name Properties \
+    --item file://initSeedData/Properties03.json \
+    --return-consumed-capacity TOTAL \
+    --return-item-collection-metrics SIZE 
+
+aws dynamodb put-item \
+    --table-name Properties \
+    --item file://initSeedData/Properties04.json \
     --return-consumed-capacity TOTAL \
     --return-item-collection-metrics SIZE 
 
 aws dynamodb put-item \
     --table-name Files \
-    --item file://initialFiles.json \
+    --item file://initSeedData/Files01.json \
+    --return-consumed-capacity TOTAL \
+    --return-item-collection-metrics SIZE
+
+aws dynamodb put-item \
+    --table-name Files \
+    --item file://initSeedData/Files02.json \
+    --return-consumed-capacity TOTAL \
+    --return-item-collection-metrics SIZE
+
+aws dynamodb put-item \
+    --table-name Files \
+    --item file://initSeedData/Files03.json \
     --return-consumed-capacity TOTAL \
     --return-item-collection-metrics SIZE
 
