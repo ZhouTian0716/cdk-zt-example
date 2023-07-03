fbb3e7a4dad52ba370a504851c81d77979d42a98
Yingxin cai <cai19980122@gmail.com>
2023-07-03 21:02:56 +1000

    delete hello lambdas

--- src/lambdas/index.ts
+++ src/lambdas/index.ts
@@ -2,15 +2,6 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda"
 import { BadRequestError, HttpError, UnexpectedError } from "./common/common"
 import { propertyDelete, propertyGetAll, propertyGetSingle, propertyPost, propertyUpdate } from "./entity/property"
 
-export const helloHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  console.log("request:", JSON.stringify(event, undefined, 2))
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: `Hello, CDK! You've hit ${event.path}\n`,
-  }
-}
-
 export const propertyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
     switch (event.httpMethod) {
